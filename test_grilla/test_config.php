53ca76211f1a3f1c0f972f7d1e970ce0cd1745d3
Marcos Piñero <marcos.pinero1976@gmail.com>
2015-12-18 17:55:39 -0300

    Paginacion en dhtmxgrid

---
+++ test_grilla/test_config.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Copyright DHTMLX LTD. http://www.dhtmlx.com
+This version of Software is free for using in non-commercial applications.
+For commercial use please contact sales@dhtmlx.com to obtain license
+*/
+ 
+    $mysql_host = "163.10.35.34";
+    $mysql_user = "root";
+    $mysql_pasw = "secyt";
+    $mysql_db   = "pampa";
+ 
+ 
+ 
+?>
