1a8cf5541cb16ec1ddc6d41125c7a6823b78da3e
Marcos Piñero <marcos.pinero1976@gmail.com>
2025-03-12 12:14:00 -0300

    varios

--- informe.cheque.php
+++ informe.cheque.php
@@ -30,23 +30,22 @@ auditarUsuarios('Informe de cheques');
 
 ?>
 <?php if(isset($_POST['ano'])) { $ano = $_POST['ano']; }else{ $ano= date('Y'); } ?>
+
 <form method="post" action="<?php echo $_SERVER['PHP_SELF']?>">
+    <?php
+    $currentYear = date("Y"); // Año actual
+    $startYear = 2010; // Año de inicio del rango
+
+
+    ?>
+
 <select size="1" name="ano">
-	<option <?php if($ano == '2010'){?> selected="selected" <?php } ?> >2010</option>
-	<option <?php if($ano == '2011'){?> selected="selected" <?php } ?> >2011</option>
-	<option <?php if($ano == '2012'){?> selected="selected" <?php } ?> >2012</option>
-	<option <?php if($ano == '2013'){?> selected="selected" <?php } ?> >2013</option>
-	<option <?php if($ano == '2014'){?> selected="selected" <?php } ?> >2014</option>
-	<option <?php if($ano == '2015'){?> selected="selected" <?php } ?> >2015</option>
-    <option <?php if($ano == '2016'){?> selected="selected" <?php } ?> >2016</option>
-    <option <?php if($ano == '2017'){?> selected="selected" <?php } ?> >2017</option>
-    <option <?php if($ano == '2018'){?> selected="selected" <?php } ?> >2018</option>
-    <option <?php if($ano == '2019'){?> selected="selected" <?php } ?> >2019</option>
-    <option <?php if($ano == '2020'){?> selected="selected" <?php } ?> >2020</option>
-    <option <?php if($ano == '2021'){?> selected="selected" <?php } ?> >2021</option>
-    <option <?php if($ano == '2022'){?> selected="selected" <?php } ?> >2022</option>
-    <option <?php if($ano == '2023'){?> selected="selected" <?php } ?> >2023</option>
-    <option <?php if($ano == '2024'){?> selected="selected" <?php } ?> >2024</option>
+    <?php for ($year = $startYear; $year <= $currentYear; $year++): ?>
+        <option value="<?php echo $year; ?>" <?php if ($ano == $year) echo 'selected="selected"'; ?>>
+            <?php echo $year; ?>
+        </option>
+    <?php endfor; ?>
+
 </select> 
 <input type="submit" value=">" name="buscar" />
 </form>
